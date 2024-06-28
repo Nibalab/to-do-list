df249e60826754cf71fca7beda824b81938e9f6a
Nibalab <abdalbakinibal@gmail.com>
2024-06-28 14:26:44 +0300

    add asignee in the js

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,30 +5,35 @@ document.addEventListener('DOMContentLoaded', () => {
 function addTask() {
     const taskInput = document.getElementById('taskInput');
     const taskDateTime = document.getElementById('taskDateTime');
+    const taskAssignee = document.getElementById('taskAssignee');
 
     const taskText = taskInput.value;
     const taskTime = taskDateTime.value;
+    const assignee = taskAssignee.value;
 
-    if (!taskText || !taskTime) {
+    if (!taskText || !taskTime || !assignee) {
         alert('Please fill out all fields.');
         return;
     }
 
     const li = document.createElement('li');
     li.className = 'pending';
-    li.innerHTML = `
-        <div class="check-icon-container">
+    li.innerHTML = ` <div class="check-icon-container">
             <svg class="check-icon" onclick="markAsCompleted(this)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></svg>
         </div>
-        <span>${taskText} <br> Due: ${taskTime}</span>
+    ${taskText} - <strong>${assignee}</strong>  
+       
+         <br> Due: ${taskTime}
     `;
+    
     li.dataset.time = taskTime;
 
     const taskList = document.getElementById('taskList');
-    taskList.appendChild(li);
 
+    taskList.appendChild(li);
     taskInput.value = '';
     taskDateTime.value = '';
+    taskAssignee.value = '';
 }
 function markAsCompleted(svg) {
     const li = svg.parentElement.parentElement; 
